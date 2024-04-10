6115b44ac038ba62c0a38d77449eff45d16b3277
Ярина Пугач <puhachyaryna@gmail.com>
2024-04-10 15:43:10 +0200

    UsedSearchBarComponent

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 export const Filter = ({
   users,
@@ -72,40 +73,11 @@ export const Filter = ({
           </a>
         ))}
       </p>
-
-      <div className="panel-block">
-        <p className="control has-icons-left has-icons-right">
-          <input
-            data-cy="SearchField"
-            type="text"
-            className="input"
-            placeholder="Search"
-            value={selectedProduct}
-            onChange={e => {
-              handleInput(e.target.value);
-            }}
-          />
-
-          <span className="icon is-left">
-            <i className="fas fa-search" aria-hidden="true" />
-          </span>
-
-          {selectedProduct && (
-            <span className="icon is-right">
-              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-              <button
-                data-cy="ClearButton"
-                type="button"
-                className="delete"
-                onClick={() => {
-                  setSelectedProduct('');
-                }}
-              />
-            </span>
-          )}
-        </p>
-      </div>
-
+      <SearchBar
+        handleInput={handleInput}
+        selectedProduct={selectedProduct}
+        setSelectedProduct={setSelectedProduct}
+      />
       <div className="panel-block is-flex-wrap-wrap">
         <a
           href="#/"
